6c60f4a7bb560a3ea90e6e433f84ce348608465a
TalkyPlace24061 <agn.tsmth42@gmail.com>
2026-01-22 19:51:03 -0500

    spindexer speed can be changed through elstic

--- src/main/java/frc/robot/subsystems/Spindexer.java
+++ src/main/java/frc/robot/subsystems/Spindexer.java
@@ -6,21 +6,27 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix6.hardware.TalonFX;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Spindexer extends SubsystemBase {
   public TalonFX m_Spindexer = new TalonFX(3);
+  public double speed = .2;
   /** Creates a new Spindexer. */
   public Spindexer() {
-
-  }
-
-  public void loadShooter(){
-    m_Spindexer.set(.2);
+    SmartDashboard.putNumber("Spindexer Speed", speed);
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    
+    speed = SmartDashboard.getNumber("Spindexer Speed", speed);
+    SmartDashboard.putNumber("Speed", speed);
   }
+
+  public void loadShooter(){
+    m_Spindexer.set(speed);
+  }
+
 }
