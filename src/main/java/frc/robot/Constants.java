93f0bd4f83c168be04c165e7667cade41dd7a972
LZRMonkey249 <logan.z.ramirez@gmail.com>
2026-02-03 20:46:09 -0500

    Added enum and variable to change constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -10,4 +10,13 @@ import static edu.wpi.first.units.Units.*;
 public final class Constants {
     public static double offsetInMeters = Meters.of(.5).in(Meters);
     public static double targetDistanceToTag = Meters.of(2.7).in(Meters);
+    public static RobotType robotType = RobotType.V0;
+
+    public enum RobotType{
+        V0,
+        V1,
+        V2,
+        V3,
+        V4
+    }
 }
