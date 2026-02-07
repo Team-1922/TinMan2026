352b4b73d94b4f197f62faf6c5122380ec891af7
TalkyPlace24061 <agn.tsmth42@gmail.com>
2026-02-07 18:36:30 -0500

    Updated the parellel command group

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -91,7 +91,7 @@ public class RobotContainer {
         DriverController.x().whileTrue(autoAlign);
         DriverController.rightTrigger().whileTrue(shoot).whileFalse(stopShooter);
         DriverController.leftTrigger().whileTrue(feed);
-        DriverController.rightBumper().whileTrue( new ParallelCommandGroup(autoAlign, shoot));
+        DriverController.rightBumper().whileTrue( new ParallelCommandGroup(autoAlign, shoot, feed, loadShooter));
 
         // Run SysId routines when holding back/start and X/Y.
         // Note that each routine should be run exactly once in a single log.
