89fad64f58b2ff075a24f96d55dc9a9382c27919
TalkyPlace24061 <agn.tsmth42@gmail.com>
2026-03-05 19:38:14 -0500

    Updated shooter values

--- src/main/java/frc/robot/commands/Shoot.java
+++ src/main/java/frc/robot/commands/Shoot.java
@@ -17,7 +17,7 @@ public class Shoot extends Command {
   private final Shooter m_shooter;
   private double m_spindexerRps = 21.7;
   private double m_feederRps = 50;
-  private double m_shooterRps = 20;
+  private double m_shooterRps = 23;
   private double m_yawThreshold = .01;
   private final Spindexer m_spindexer;
   private final Feeder m_feeder;
