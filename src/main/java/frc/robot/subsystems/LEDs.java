1476f49efbee5937daed98910f524cf88210f5c3
TalkyPlace24061 <agn.tsmth42@gmail.com>
2026-03-06 18:42:39 -0500

    Added an LEDs subsystem

---
+++ src/main/java/frc/robot/subsystems/LEDs.java
@@ -0,0 +1,27 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class LEDs extends SubsystemBase {
+  /** Creates a new LEDs. */
+  public LEDs() {
+  final AddressableLED m_led = new AddressableLED(1);
+
+  final AddressableLEDBuffer m_ledBuffer = new AddressableLEDBuffer(0);
+    m_led.setLength(m_ledBuffer.getLength());
+  
+    m_led.setData(m_ledBuffer);
+    m_led.start();
+    }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
