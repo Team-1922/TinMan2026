efd4702642cb3d8b5448c27de9f0bf181909f3a9
TalkyPlace24061 <agn.tsmth42@gmail.com>
2026-03-07 17:34:25 -0500

    Added on and off times for the hub to signaling

--- src/main/java/frc/robot/subsystems/Signaling.java
+++ src/main/java/frc/robot/subsystems/Signaling.java
@@ -4,7 +4,10 @@
 
 package frc.robot.subsystems;
 
+import java.util.Optional;
+
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.GenericHID.RumbleType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -12,16 +15,77 @@ import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
 public class Signaling extends SubsystemBase {
   /** Creates a new Signaling. */
+  private String m_gameData = DriverStation.getGameSpecificMessage();
+  double m_matchTime = DriverStation.getMatchTime();
   private final CommandXboxController DriverController = new CommandXboxController(0);
+  
   public Signaling() {}
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    if(m_gameData.length() > 0){
+      switch (m_gameData.charAt(0)){
+        case 'B' :
+          break;
+        case 'R' :
+          break;
+        default :
+          break;
+      }
+    }
   }
 
   public void startRotatingBack(){
     
       DriverController.setRumble(RumbleType.kBothRumble, 1);
   }
+
+  private boolean isHubActive(){
+    Optional <Alliance> alliance = DriverStation.getAlliance();
+    if(alliance.isEmpty()){
+      return false;
+    }
+
+    if(DriverStation.isAutonomousEnabled()){
+      return true;
+    }
+
+    if(!DriverStation.isTeleopEnabled()){
+      return false;
+    }
+
+    if(m_gameData.isEmpty()){
+      return true;
+    }
+
+    boolean redInactiveFirst = false;
+      switch (m_gameData.charAt(0)){
+        case 'R' -> redInactiveFirst = true;
+        case 'B' -> redInactiveFirst = false;
+        default -> {
+          return true;
+        }
+      }
+
+      boolean shift1Active = switch (alliance.get()) {
+        case Red -> !redInactiveFirst;
+        case Blue -> redInactiveFirst;
+      };
+
+      if(m_matchTime > 130 ) {
+        return true;
+      } else if (m_matchTime > 105) {
+        return shift1Active;
+      } else if (m_matchTime > 80) {
+        return !shift1Active;
+      } else if (m_matchTime > 55) {
+        return shift1Active;
+      } else if (m_matchTime > 30) {
+        return !shift1Active;
+      } else {
+        return true;
+      }
+  }
+
 }
