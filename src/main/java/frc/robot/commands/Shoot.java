177c568f8ec47280a57ae1464393cca6b8875bc4
Jesse Vendt <jvendt@ebsco.com>
2026-03-12 23:48:50 -0400

    Adjusting spindexer rps (#54)

--- src/main/java/frc/robot/commands/Shoot.java
+++ src/main/java/frc/robot/commands/Shoot.java
@@ -16,7 +16,7 @@ import frc.robot.subsystems.Localization;
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class Shoot extends Command {
   private final Shooter m_shooter;
-  private double m_spindexerRps = 8;
+  private double m_spindexerRps = 20;
   private double m_feederRps = 100;
   private double m_shooterRps = 20;
   private double m_shuttleRps = 30;
