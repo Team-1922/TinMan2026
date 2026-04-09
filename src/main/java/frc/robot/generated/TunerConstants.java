74864f0cddd58ca87e0df687a85480d18fc7c3b5
TalkyPlace24061 <agn.tsmth42@gmail.com>
2026-04-09 18:43:46 -0400

    Chsnged drive ratio and steer ratio

--- src/main/java/frc/robot/generated/TunerConstants.java
+++ src/main/java/frc/robot/generated/TunerConstants.java
@@ -102,8 +102,8 @@ public class TunerConstants {
     // This may need to be tuned to your individual robot
     private static final double kCoupleRatio = 3.125;
 
-    private static final double kDriveGearRatio = 7.125;
-    private static final double kSteerGearRatio = 18.75;
+    private static final double kDriveGearRatio = 6.75;
+    private static final double kSteerGearRatio = 12.8;
     private static final Distance kWheelRadius = Inches.of(2);
 
     private static final boolean kInvertLeftSide = false;
